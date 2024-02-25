219842bceb2c3f9303ca4dfbcb8f486137a5b252
Nikita <tazxcc13@gmail.com>
2024-02-25 17:56:10 +0200

    1

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from '../img/logo-name.svg'
+import Button from './Button'
+import './Header.css'
+
+export default function Header(){
+    return(
+        <header>
+            <a href="@"><img src={logo} alt="RateMyStudy" /></a>
+            <Button/>
+        </header>
+    )
+}
