e7306b7b1c5a1c72e5d698ac66798d03d1ea8dc6
Nikita <tazxcc13@gmail.com>
2024-02-26 12:26:16 +0200

    1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Header from './components/Header'
-import BodyMain from './components/BodyMain'
+
 
 
 function App() {
   return (
     <div>
       <Header/>
-      <BodyMain/>
+      
+      <main>
+        <div className='mainbg main_search'>
+          <div className='slog'>
+            <h1>Ваш сервис для поиска отзывов</h1>
+            <form action="">
+              <input className='search' type="text" placeholder='Найдите ваш университет'/>
+            </form>
+          </div>
+        </div>
+      </main>
     </div>  
   );
 }
