ceecd220d1874e8fe4985fbf0e1ee14ca29a272e
Nikita <tazxcc13@gmail.com>
2024-03-01 16:58:43 +0200

    1

--- client/src/components/Story.jsx
+++ client/src/components/Story.jsx
@@ -2,17 +2,7 @@ import './Story.css'
 
 
 export default function Story({title, description, photo}){
-    return(
-            // <ul>
-            //     <li>
-            //         <p>
-            //             <strong>{title} </strong>
-            //         </p>
-            //         <p>{description}</p>
-            //     </li>
-            //     <li><img className="mainphoto" src={photo} alt="" /></li>
-            // </ul>
-            
+    return(  
             <div className='task'>
                 <div className='body_task'>
                     <h3>{title}</h3>
@@ -23,13 +13,5 @@ export default function Story({title, description, photo}){
                     <img src={photo} width={260} alt="" />
                 </div>
             </div>
-
-
-            // <ul className='task'>
-            //     <li><strong>{title}</strong></li>
-            //     <p>{description}</p>
-            //     <li><img src={photo} alt="" /></li>
-            // </ul>
-
     )
 }
