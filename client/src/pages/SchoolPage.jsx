f38df912fd888d3803b357fdb044a7a733ebf1f0
Nikita <tazxcc13@gmail.com>
2024-03-26 16:33:04 +0200

    1

--- client/src/pages/SchoolPage.jsx
+++ client/src/pages/SchoolPage.jsx
@@ -3,17 +3,17 @@ import './style/SchoolPage.css'
 
 
 const SchoolPage = () =>{
-    const school = {id: 1, name: 'КИТиС', review: 26, rating: 4.5}
+    const school = {id: 1, name: 'КИТиС', review: 26, rating: 4.5, img: 'https://сопк.рф/wp-content/uploads/2019/12/Kolledzh-informatsionnyh-tehnologij-i-stroitelstva-Kaliningrad.jpg'}
     return(
         <div className="main_schoolPage">
-            <div className="school">
+            <div className="school" >
                 <h1>{school.name}</h1>
                 <p>Оценка: {school.rating}</p>
-                <h2>Всего отызвов {school.review}</h2>
+                <h2>Всего отызвов: {school.review}</h2>
             </div>
             <div className="formForRev">
                 <form action="">
-                    <input type="text" placeholder="Оставить отзыв"/>
+                    <input type="text" className="rev_input" placeholder="Оставить отзыв"/>
                     <input type="button" value="Отправить" className="form_btn" />
                 </form>
             </div>
--- client/src/store/SchoolStore.js
+++ client/src/store/SchoolStore.js
@@ -8,18 +8,15 @@ export default class SchoolStore{
             {id: 3, name: 'Колледжи'},
         ]
         this._institution = [
-            {id: 1, name: 'КИТиС', review: 6, rating: 4.5},
-            {id: 2, name: 'БФУ', rating: 4.9},
-            {id: 3, name: 'КГТУ', rating: 5},
-            {id: 3, name: 'КГТУ', rating: 5},
-            {id: 3, name: 'КГТУ', rating: 5},
-            {id: 3, name: 'КГТУ', rating: 5},
-            {id: 3, name: 'КГТУ', rating: 5},
-            {id: 3, name: 'КГТУ', rating: 5},
-            {id: 3, name: 'КГТУ', rating: 5},
-            {id: 3, name: 'КГТУ', rating: 5},
-            {id: 3, name: 'КГТУ', rating: 5},
-            {id: 3, name: 'КГТУ', rating: 5},
+            {id: 1, name: 'КИТиС', review: 6, rating: 4.5, img: 'https://сопк.рф/wp-content/uploads/2019/12/Kolledzh-informatsionnyh-tehnologij-i-stroitelstva-Kaliningrad.jpg'},
+            {id: 2, name: 'БФУ', rating: 4.9, img: 'https://studyinrussia.ru/upload/iblock/79c/79c6fc400b537388d57c4579fc5be6d6.jpg'},
+            {id: 3, name: 'КГТУ', rating: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kaliningrad_05-2017_img51_Technical_University.jpg'},
+            {id: 3, name: 'КГТУ', rating: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kaliningrad_05-2017_img51_Technical_University.jpg'},
+            {id: 3, name: 'КГТУ', rating: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kaliningrad_05-2017_img51_Technical_University.jpg'},
+            {id: 3, name: 'КГТУ', rating: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kaliningrad_05-2017_img51_Technical_University.jpg'},
+            {id: 3, name: 'КГТУ', rating: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kaliningrad_05-2017_img51_Technical_University.jpg'},
+            {id: 3, name: 'КГТУ', rating: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kaliningrad_05-2017_img51_Technical_University.jpg'},
+            {id: 3, name: 'КГТУ', rating: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kaliningrad_05-2017_img51_Technical_University.jpg'},
         ]
         makeAutoObservable(this)
     }
