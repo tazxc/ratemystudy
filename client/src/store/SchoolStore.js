59d0947d7bb000ccaeafc4dc3ba51aee76d79665
Nikita <tazxcc13@gmail.com>
2024-03-26 16:50:55 +0200

    1

--- client/src/store/SchoolStore.js
+++ client/src/store/SchoolStore.js
@@ -11,12 +11,11 @@ export default class SchoolStore{
             {id: 1, name: 'КИТиС', review: 6, rating: 4.5, img: 'https://сопк.рф/wp-content/uploads/2019/12/Kolledzh-informatsionnyh-tehnologij-i-stroitelstva-Kaliningrad.jpg'},
             {id: 2, name: 'БФУ', rating: 4.9, img: 'https://studyinrussia.ru/upload/iblock/79c/79c6fc400b537388d57c4579fc5be6d6.jpg'},
             {id: 3, name: 'КГТУ', rating: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kaliningrad_05-2017_img51_Technical_University.jpg'},
-            {id: 3, name: 'КГТУ', rating: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kaliningrad_05-2017_img51_Technical_University.jpg'},
-            {id: 3, name: 'КГТУ', rating: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kaliningrad_05-2017_img51_Technical_University.jpg'},
-            {id: 3, name: 'КГТУ', rating: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kaliningrad_05-2017_img51_Technical_University.jpg'},
-            {id: 3, name: 'КГТУ', rating: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kaliningrad_05-2017_img51_Technical_University.jpg'},
-            {id: 3, name: 'КГТУ', rating: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kaliningrad_05-2017_img51_Technical_University.jpg'},
-            {id: 3, name: 'КГТУ', rating: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kaliningrad_05-2017_img51_Technical_University.jpg'},
+            {id: 4, name: 'КГТУ', rating: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kaliningrad_05-2017_img51_Technical_University.jpg'},
+            {id: 5, name: 'КГТУ', rating: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kaliningrad_05-2017_img51_Technical_University.jpg'},
+            {id: 6, name: 'КГТУ', rating: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kaliningrad_05-2017_img51_Technical_University.jpg'},
+            {id: 7, name: 'КГТУ', rating: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kaliningrad_05-2017_img51_Technical_University.jpg'},
+            {id: 8, name: 'КГТУ', rating: 5, img: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Kaliningrad_05-2017_img51_Technical_University.jpg'},
         ]
         makeAutoObservable(this)
     }
