b9bdd92488590aaee0839840df2d76725e130e2a
Nikita <tazxcc13@gmail.com>
2024-03-27 16:06:43 +0200

    schoolpage(надо после серверной части сделать стили)

--- client/src/pages/SchoolPage.jsx
+++ client/src/pages/SchoolPage.jsx
@@ -6,8 +6,8 @@ const SchoolPage = () =>{
     const school = {id: 1, name: 'КИТиС', review: 26, rating: 4.5, img: 'https://сопк.рф/wp-content/uploads/2019/12/Kolledzh-informatsionnyh-tehnologij-i-stroitelstva-Kaliningrad.jpg'}
     return(
         <div className="main_schoolPage">
-            <div className="school" >
-                <h1>{school.name}</h1>
+            <div className="school">
+                <div className="nameSchool"><h1>{school.name}</h1></div>
                 <p>Оценка: {school.rating}</p>
                 <h2>Всего отызвов: {school.review}</h2>
             </div>
@@ -16,6 +16,21 @@ const SchoolPage = () =>{
                     <input type="text" className="rev_input" placeholder="Оставить отзыв"/>
                     <input type="button" value="Отправить" className="form_btn" />
                 </form>
+            </div>
+            
+            <div className="allReview">
+                <div className="AllScore">
+                    <div className="score">4.5</div>
+                    <div>Rating school</div>
+                </div>
+                <div className="review">
+                    <div className="reviewCommit">
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium animi id, autem debitis qui vitae, 
+                            eius molestias consectetur, 
+                            quia culpa ea unde minima veritatis inventore doloremque odit rem dignissimos? Molestiae!</p>
+                    </div>
+                </div>
+                
             </div>
         </div>
     )
