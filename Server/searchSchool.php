08b5b54850678cb20397e8a923c645408e621973
Nikita <tazxcc13@gmail.com>
2024-05-04 15:40:12 +0200

    1

--- Server/searchSchool.php
+++ Server/searchSchool.php
@@ -5,9 +5,10 @@ require_once __DIR__.'/boot.php';
 $query = isset($_GET['query']) ? $_GET['query'] : '';
 
 // Поиск школ в базе данных
-$stmt = pdo()->prepare("SELECT name FROM school WHERE name LIKE :query LIMIT 3");
+$stmt = pdo()->prepare("SELECT id, name, fullName FROM school WHERE name LIKE :query LIMIT 3");
 $stmt->execute(array(':query' => "%$query%"));
 $schools = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 
 // Возвращаем результат в формате JSON
 header('Content-Type: application/json');
